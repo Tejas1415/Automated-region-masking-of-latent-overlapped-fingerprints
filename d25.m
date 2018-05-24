0a14cf4be372fe21e5448f1b5a30b68a798690d3
Tejas Krishna Reddy <37299405+Tejas1415@users.noreply.github.com>
2018-05-25 00:32:30 +0530

    Update d25.m

--- d25.m
+++ d25.m
@@ -1,35 +1,50 @@
+% Code by Tejas K
+% Jan - Feb 2017
+
+% I have explained most of the unfamiliar commands but to know why I am doing these, Please go through the paper attached. 
+% Please dont forget to cite the paper at https://ieeexplore.ieee.org/abstract/document/8245111/    
+
+
+% Clear memories and other open windows. Ready for the algo to function properly.
 clc
 clear all
 close all
 
-tic
-I4=imread('d25.jpg');
+tic     % to check the time of execution
+I4=imread('d25.jpg');              % Read an image, if its already gray then dont need to convert, else convert.
 %I4=rgb2gray(I);
-I2 = padarray(I4,[30, 30],255);
-I3=im2double(I2);
+I2 = padarray(I4,[30, 30],255);    % It will add a thick white ( 255 ) border for the input image. 
+I3=im2double(I2);                  % Pixel Values from 0-255 and not 0-1. 
 figure
-imshow(I2); title('I2');
+imshow(I2); title('I2');           % display
 
-f1= fspecial('average',20);
-Y=filter2(f1,I2,'same');
+f1= fspecial('average',20);        % Blurring with an averaging filter. (Learn concepts of blurring to get indepth understanding)
+Y=filter2(f1,I2,'same');           % apply that 20x20 filter on the image to blur it.
 figure
-imshow(Y/255);
+imshow(Y/255);                     % Display blurred image
 title('Y/255');
 
-A=ones(size(Y));
-H=A-(Y/255);
+A=ones(size(Y));                   % create a white image of the same size.
+H=A-(Y/255);                       % subtract blurred image with the pure white image.
 
-H12=filter2(f1,H,'valid');
+#The above step is to detect edges. U can use canny/sobel but canny produces edgesmore than the requirement
+# sobel producess less than the requirement.
+# bluring and subtracting method we can optimise the amount of blurring that we need.
+
+H12=filter2(f1,H,'valid'); 
 H2=imresize(H12,size(I3));
 figure,imshow(3*H2);title('3*h2');
 H3=3.1*H2;
+
+# check the IEEE paper in readme to understand why it was multiplied by a constant. 
+# vary the constant according to the type of input image.
 
 [x, y] = size(H2);
 
 H4= zeros(x,y);
 for i=1:x
     for j=1:y
-      if H3(i,j) > 0.9
+      if H3(i,j) > 0.9                  % applying threshold and copying the contents above threshold to a seperate image.
          H4(i,j) = H3(i,j);
       end
     end
@@ -37,21 +52,22 @@ end
 
 figure,imshow(H4);title('h4');
 
-BW3=H4;
+
+BW3=H4;                                 % creating a new variable.
 %dilate H4
 H5=edge(H4,'sobel');
-BW4 = bwpropfilt(H5,'perimeter',1);
-BW4=imdilate(BW4,ones(3,3));
+BW4 = bwpropfilt(H5,'perimeter',1);     %  The highest perimiter stays, rest get deleted
+BW4=imdilate(BW4,ones(3,3));            % little dialation, to connect lines with tiny gaps in between them.
 figure,imshow(BW4);title('bw4');
 
-H8=imfill(BW4,'holes');
+H8=imfill(BW4,'holes');                 % fills all connected components in the image. Any loop is filled with white color.
 figure,imshow(H8);title('H8');
 
 [x, y]=size(I2);
 H4= zeros(x,y);
 for i=1:x
     for j=1:y
-      if I2(i,j) > 254
+      if I2(i,j) > 254                   % Threshold and create a new image
          H4(i,j) = I2(i,j);
       end
     end
@@ -59,7 +75,7 @@ end
 figure,imshow(H4);title('H4');
 
 BW2=H4;
-BW8=bwareaopen(BW2,5);
+BW8=bwareaopen(BW2,5);                  # Elements with less than 5 pixels (basically dots) are deleted.
 BW2=imdilate(BW8,ones(7,7));
 figure,imshow(BW2);title('BW2');
 BW2=im2double(BW2);
@@ -131,6 +147,8 @@ end
 I2=im2double(I2);
 figure,imshow(Z);title('improved z');
 
-Z7=ssim(I3,Z);
-Z8=psnr(I3,Z);
-k1=toc;
+
+% Validation of Results
+Z7=ssim(I3,Z);                  % Structure Similarity Check - value should be close to zero
+Z8=psnr(I3,Z);                  % PSNR ratio. - value shoud be close to infinity
+k1=toc;                         % end the time module to check the total time for executing the algorithm.
